f99f67c32dc171b06c74f032206636fee403f5d0
manasaananad <142892071+manasaananad@users.noreply.github.com>
2026-05-01 22:30:59 +0530

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -1,1 +1,14 @@
+const mongoose = require("mongoose");
 
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  phone: String,
+  interest: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("User", UserSchema);
